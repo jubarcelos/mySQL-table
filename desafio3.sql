be0cb9bb1b4108e19d7c44937c11d0fe849edb7d
Julia Barcelos <julaai@gmail.com>
2022-03-10 12:29:23 -0300

    To show a table with totaly of music and time of duration by client, I used INNERJOIN to get two table together and handle all informations I needed.

---
+++ desafio3.sql
@@ -0,0 +1,7 @@
+SELECT client AS 'usuario',
+  COUNT(h.song_id) AS 'qtde_musicas_ouvidas',
+  ROUND (SUM((s.duration)/60),2) AS 'total_minutos'
+FROM SpotifyClone.historic h
+INNER JOIN SpotifyClone.clients c ON c.client_id = h.client_id
+INNER JOIN SpotifyClone.songs s ON s.song_id = h.song_id
+GROUP BY c.client_id ORDER BY c.client;
